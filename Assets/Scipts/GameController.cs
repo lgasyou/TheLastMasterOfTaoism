5217e7706a659f4e5a28f6a2db54974145fdc4b1
Li Zeqing <lgasyou@163.com>
2017-10-19 11:12:43 +0800

    添加README.md，简要完成鼠标左键点击事件

---
+++ Assets/Scipts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            OnLeftMouseDown();
+        }
+    }
+
+    void OnLeftMouseDown() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            print("OnLeftMouseDown");
+            if (hit.collider.gameObject.tag == "Building") {
+
+            }
+        }
+    }
+}
