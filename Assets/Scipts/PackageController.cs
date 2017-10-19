fd06735915b983db88335faffc3c217e994eacbd
Li Zeqing <lgasyou@163.com>
2017-10-19 11:37:52 +0800

    添加物品与背包管理系统

---
+++ Assets/Scipts/PackageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Item {
+    public string name;
+    public string decription;
+    public bool useable;
+}
+
+class Comsumable : Item {
+    public int useTime;
+    public int maxUseTime;
+    public string kind;
+}
+
+public class PackageController : MonoBehaviour {
+    Dictionary<string, Item> items;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
