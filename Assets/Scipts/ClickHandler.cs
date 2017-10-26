7546fcd315923b7dafcb6a142d7a754773031250
Li Zeqing <lgasyou@163.com>
2017-10-26 14:57:34 +0800

    修改按键逻辑

--- Assets/Scipts/ClickHandler.cs
+++ Assets/Scipts/ClickHandler.cs
@@ -18,8 +18,84 @@ public class ClickHandler : MonoBehaviour {
         // $number
         string withoutRightParen = withoutLeftParen.Substring(0, withoutLeftParen.Length - 1);
         int index = int.Parse(withoutRightParen);
-        System.Reflection.MethodInfo methodInfo = typeof(MoveExecuter).GetMethod(splited[0]);
-        methodInfo.Invoke(GetComponent<MoveExecuter>(), new object[] { index });
+        System.Reflection.MethodInfo methodInfo = typeof(ClickHandler).GetMethod(splited[0] + "In" + "Store");
+        methodInfo.Invoke(this, new object[] { index });
+    }
+
+    public void BagButtonInStore(int indexInPackage) {
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        if (playerObj == null) {
+            Debug.Log("Cannot find player");
+            return;
+        }
+        Entity.Player player = playerObj.GetComponent<Entity.Player>();
+        PackageItem item = player.package.Get(indexInPackage);
+        if (item != null) {
+            if (!player.equipment.IsFull()) {
+                player.equipment.Put(item);
+                player.package.Remove(indexInPackage);
+            }
+        }
+        print("Store");
+    }
+
+    public void BagButtonInBackpack(int indexInPackage) {
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        if (playerObj == null) {
+            Debug.Log("Cannot find player");
+            return;
+        }
+        Entity.Player player = playerObj.GetComponent<Entity.Player>();
+        PackageItem item = player.package.Get(indexInPackage);
+        if (item != null) {
+            if (!player.equipment.IsFull()) {
+                player.equipment.Put(item);
+                player.package.Remove(indexInPackage);
+            }
+        }
+        print("Backpack");
+    }
+
+    public void EquipmentButton(int indexInEquipment) {
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        if (playerObj == null) {
+            Debug.Log("Cannot find player");
+            return;
+        }
+        Entity.Player player = playerObj.GetComponent<Entity.Player>();
+        PackageItem item = player.equipment.Get(indexInEquipment);
+        if (item != null) {
+            if (!player.package.IsFull()) {
+                player.package.Put(item);
+                player.equipment.Remove(indexInEquipment);
+            }
+        }
+        print(indexInEquipment);
+    }
+
+    public void StoreButton(int indexInStore) {
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        if (playerObj == null) {
+            Debug.Log("Cannot find player");
+            return;
+        }
+        Entity.Player player = playerObj.GetComponent<Entity.Player>();
+
+        GameObject sellerObject = GameObject.FindWithTag("NPC");
+        if (sellerObject == null) {
+            Debug.Log("Cannot find seller");
+            return;
+        }
+        Entity.TransactionPoint point = sellerObject.GetComponent<Entity.TransactionPoint>();
+
+        PackageItem item = point.package.Get(indexInStore);
+        if (item != null) {
+            if (!player.package.IsFull()) {
+                player.package.Put(item);
+                point.package.Remove(indexInStore);
+            }
+        }
+        print(indexInStore);
     }
 
 }
--- Assets/Scipts/Entity/TransactionPoint.cs
+++ Assets/Scipts/Entity/TransactionPoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Entity {
     // 交易点
     public class TransactionPoint : Creative {
-        Package package = new Package();    // 可交易物品列表
+        public Package package = new Package();    // 可交易物品列表
 
         // Use this for initialization
         void Start() {
